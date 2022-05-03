3145b31a88df6782334d3abb2076a64c74e95a9d
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2022-05-03 15:57:58 -0300

    feat: created SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -22,6 +23,7 @@ const Home: NextPage = () => {
           Get access to all the publications <br />
           <span>for $9.90 month</span>
         </p>
+        <SubscribeButton />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding" />
